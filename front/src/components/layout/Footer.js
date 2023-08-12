ffb43dde853f2e67518f5c6f5dd4a0bf9f3ca16f
Lexx R <alexcumbal@gmail.com>
2023-08-12 05:41:09 -0500

    Initial project load

---
+++ front/src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+export const Footer = () => {
+  return (
+    <Fragment>
+        <footer className="py-1">
+            <p className="text-center mt-1">
+                Vety Shop Store - 2023 - Academic Project - Natalí Velásquez - Todos los derechos reservados
+            </p>
+        </footer>
+
+    </Fragment>
+  )
+}
